6b1b4ec047b0334a38018b261ffaff6f85fcb50b
fencim <fencim@gmail.com>
2021-05-18 10:54:29 +0800

    migrage standard vue to quasar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,47 +1,40 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <input type="text" v-model="applicant" > 
+        <div @click="addApplicant">Add  {{applicant}}</div>
+        <ul>
+            <li v-for="student in students" >{{student.name}} Gwapo</li>
+        </ul>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ClassComponent.vue';
 import { Vue, Component } from 'vue-property-decorator';
-
-@Component({
-  components: { ExampleComponent }
-})
+interface IPlayers {
+  [member:string]: number;
+}
+@Component
 export default class PageIndex extends Vue {
-  todos: Todo[] = [
-    {
-      id: 1,
-      content: 'ct1'
-    },
-    {
-      id: 2,
-      content: 'ct2'
-    },
-    {
-      id: 3,
-      content: 'ct3'
-    },
-    {
-      id: 4,
-      content: 'ct4'
-    },
-    {
-      id: 5,
-      content: 'ct5'
-    }
-  ];
-  meta: Meta = {
-    totalCount: 1200
-  };
+   applicant = "";
+   scoring:IPlayers = {
+      'Usman': 0,
+      'Matin': 0,
+      'Felix': 0,
+      'Magongcar': 0,
+      'Omair': 0                
+   };
+   get students(){
+      return Object.keys(this.scoring)
+          //map string to object {name}
+          .map((studentName) => {
+              return {
+                  name: studentName
+              }
+          })
+  }
+  addApplicant(){
+      this.scoring[this.applicant] = 0;
+      this.$forceUpdate();
+  }
 };
 </script>
